d52941cc9925be81f3668e681057ad39e8d44b48
katopz <katopz@gmail.com>
2026-09-09 22:42:02 +0700

    cargo-heal 0.1.4

--- Formula/cargo-heal.rb
+++ Formula/cargo-heal.rb
@@ -3,24 +3,24 @@
 class CargoHeal < Formula
   desc "Modelless code healer: clippy, rust perf, kernel, docker, rustc-error fixes"
   homepage "https://github.com/gist-rs/cargo-heal"
-  version "0.1.3"
+  version "0.1.4"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/gist-rs/cargo-heal/releases/download/v0.1.3/cargo-heal-v0.1.3-aarch64-apple-darwin.tar.gz"
-      sha256 "04801e439ab2fe17274407cae846f48b27f0b15b69da9fb37af4c7d142ef0bd6"
+      url "https://github.com/gist-rs/cargo-heal/releases/download/v0.1.4/cargo-heal-v0.1.4-aarch64-apple-darwin.tar.gz"
+      sha256 "a6965b525f8ae9df3296c51fc62afe6e5639210197e65ef178b86c66dc5109a5"
     else
-      url "https://github.com/gist-rs/cargo-heal/releases/download/v0.1.3/cargo-heal-v0.1.3-x86_64-apple-darwin.tar.gz"
-      sha256 "ecdab40a0aab326ef487c821941b364c7539fa38e73998f2f73de05baf39a7f4"
+      url "https://github.com/gist-rs/cargo-heal/releases/download/v0.1.4/cargo-heal-v0.1.4-x86_64-apple-darwin.tar.gz"
+      sha256 "f5faaee297f1734aa338288dfec83eedaf5d56dfbaa8b8deb879d5730c6e3469"
     end
   end
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/gist-rs/cargo-heal/releases/download/v0.1.3/cargo-heal-v0.1.3-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "44876bea4c74b37ac044581fe50bd21be9e41a0e448e54206fc87ec7a17062d5"
+      url "https://github.com/gist-rs/cargo-heal/releases/download/v0.1.4/cargo-heal-v0.1.4-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "23a2df1a72b6959b3e4c550d7739a52194b166d7b7e285444bb4d23635a8ae0d"
     else
-      url "https://github.com/gist-rs/cargo-heal/releases/download/v0.1.3/cargo-heal-v0.1.3-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "195b1f263c99ddbdcc76882b6385a027694d198676a4cf9e294a6c34b934741b"
+      url "https://github.com/gist-rs/cargo-heal/releases/download/v0.1.4/cargo-heal-v0.1.4-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "747fbd8a7acf427b3f79b261eb7a663f9f942c9213953447649ed6b883da4d46"
     end
   end
 
